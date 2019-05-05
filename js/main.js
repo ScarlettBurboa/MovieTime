ceeb65570367651b47d1a771e5ca1593ea65c0e9
ScarlettBurboa <scarlett.burboa@gmail.com>
2019-05-05 00:55:13 -0400

    eliminando dependiencia node font awesome

--- js/main.js
+++
@@ -1,5 +0,0 @@
-window.onload = () =>{
-    document.getElementById('home').style.display = 'block';
-    document.getElementById('options').style.display = 'none';
-    document.getElementById('result').style.display = 'none';
-}
