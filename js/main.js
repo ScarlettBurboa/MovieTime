093f32f9e5e6592d17fd3aa3982f752eeb92f429
ScarlettBurboa <scarlett.burboa@gmail.com>
2019-05-05 00:39:55 -0400

    agregando identidad mas favicon

---
+++ js/main.js
@@ -0,0 +1,5 @@
+window.onload = () =>{
+    document.getElementById('home').style.display = 'block';
+    document.getElementById('options').style.display = 'none';
+    document.getElementById('result').style.display = 'none';
+}
