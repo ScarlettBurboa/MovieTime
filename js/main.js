9e4788c0e6cc9ba320e9bd56339d8737428d91b0
ScarlettBurboa <scarlett.burboa@gmail.com>
2019-05-05 22:11:01 -0400

    Agregando estructura y css a seccción resultado

--- js/main.js
+++ js/main.js
@@ -9,3 +9,9 @@ const play = document.getElementById('play').addEventListener('click', () => {
   document.getElementById('options').style.display = 'flex';
   document.getElementById('result').style.display = 'none';
 });
+
+const selection = document.getElementById('min30').addEventListener('click', () => {
+  document.getElementById('home').style.display = 'none';
+  document.getElementById('options').style.display = 'none';
+  document.getElementById('result').style.display = 'block';
+});
